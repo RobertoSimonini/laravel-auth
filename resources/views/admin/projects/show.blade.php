53bdee6dc8e12d1b5dfea24ad22f612338e046b0
RobertoSimonini <robertosimonini1@gmail.com>
2023-03-09 15:50:16 +0100

    fixed show img_path

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -14,7 +14,7 @@
     <div class="card h-100 mb-3">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="{{$project->image}}" class="img-fluid rounded-start" alt="...">
+            <img src="{{asset('storage/' . $project->image)}}" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8 text-center">
             <div class="card-body">
